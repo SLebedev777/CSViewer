8adc7aa36a46476afc3414d2329413f4d2b91ae5
SLebedev777 <naotlichnenko@gmail.com>
2022-09-25 01:25:54 +0300

    first test

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,5 +1,12 @@
 #include "gtest/gtest.h"
+#include "CmdLineParser.h"
+
 
 TEST(CSViewerTests, test1) {
-    EXPECT_EQ(2, 1);
+    char* argv[] = {"csviewer.exe", "test.csv"};
+    int argc = 2;
+    auto result = ParseCmdLineArgs(argc, argv);
+    CmdLineArgsParseResult expected;
+    expected.input_filename = "test.csv";
+    EXPECT_EQ(expected.input_filename, result.input_filename);
 }
