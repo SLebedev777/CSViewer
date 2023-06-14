00208313bb0ee4e941fc70986fe2caa6ff619f34
SLebedev777 <naotlichnenko@gmail.com>
2023-06-14 12:39:48 +0300

    upload libiconv 1.17 to externals

---
+++ externals/libiconv/src/include_win/config.h
@@ -0,0 +1,9 @@
+#define ICONV_CONST const
+#define HAVE_WCHAR_H 1
+#define HAVE_WCHAR_T 1
+#define HAVE_MBRTOWC 1
+#define HAVE_WCRTOMB 1
+#define HAVE_SETLOCALE 1
+#define WORDS_LITTLEENDIAN 1
+/* #undef BROKEN_WCHAR_H */
+#define USE_MBSTATE_T 1
